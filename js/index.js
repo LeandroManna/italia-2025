71215cc5e8b61b0653a3297a2e7ca6d973a7c6bd
Leandro <leandro.a.manna@gmail.com>
2025-03-11 18:55:47 -0300

    Mejoras en las actividades

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ fetch("json/viaje_italia.json")
       const vuelo = viaje.vuelos[tipo];
       vuelosContainer.innerHTML += `
                         <div class="col-md-6">
-                            <div class="card mb-3" onclick="mostrarModal('Vuelo ${tipo}', '${vuelo.imagen}','${vuelo.sla} <br> ${vuelo.aep} <br>Salida de ${vuelo.origen}: ${vuelo.horario_salida} <br> Llegada a ${vuelo.escala}: ${vuelo.llegada_escala} <br> Salida de ${vuelo.escala}: ${vuelo.salida_escala} <br> Llegada a ${vuelo.destino}: ${vuelo.horario_llegada} <br> ${vuelo.aep2} <br> ${vuelo.sla2}')">
+                            <div class="card mb-3" onclick="mostrarModal('Vuelo ${tipo}', '${vuelo.imagen}','${vuelo.sla} <br> ${vuelo.aep} <br> <b> Salida de ${vuelo.origen}:</b> ${vuelo.horario_salida} <br> <b> Llegada a ${vuelo.escala}:</b> ${vuelo.llegada_escala} <br> <b> Salida de ${vuelo.escala}:</b> ${vuelo.salida_escala} <br> <b> Llegada a ${vuelo.destino}:</b> ${vuelo.horario_llegada} <br> ${vuelo.aep2} <br> ${vuelo.sla2}')">
                                 <div class="card-body">
                                     <h5 class="card-title">Vuelo ${tipo}</h5>
                                     <p class="card-text">Origen: ${vuelo.origen} - Destino: ${vuelo.destino}</p>
@@ -23,11 +23,11 @@ fetch("json/viaje_italia.json")
     viaje.alojamiento.forEach((aloja) => {
       alojamientoContainer.innerHTML += `
           <div class="col-md-4">
-              <div class="card mb-3" onclick="mostrarModal('Alojamiento en ${aloja.ciudad}', '${aloja.imagen}', 'Check-in: ${aloja.check_in} <br> Check-out: ${aloja.check_out} <br> Noches: ${aloja.noches} <br> Ubicación: ${aloja.ubicacion} <br> <a href= ${aloja.link} target= ${aloja.target}>Link</a> <br> Precio: ${aloja.precio}')">
+              <div class="card mb-3" onclick="mostrarModal('Alojamiento en ${aloja.ciudad}', '${aloja.imagen}', '<b>Check-in:</b> ${aloja.check_in} <br> <b>Check-out:</b> ${aloja.check_out} <br> <b>Noches:</b> ${aloja.noches} <br> <b>Ubicación:</b> ${aloja.ubicacion} <br> <a href= ${aloja.link} target= ${aloja.target}>Link</a> <br> <b>Precio:</b> ${aloja.precio}')">
                 <div class="card-body d-flex align-items-center">
                   <div class="flex-grow-1">
                       <h5 class="card-title">${aloja.ciudad}</h5>
-                      <p class="card-text">${aloja.noches} noches <br> ${aloja.precio} total</p>
+                      <p class="card-text"><mark>${aloja.noches} noches</mark> <br> ${aloja.precio} total</p>
                   </div>
                   <div>
                       <img src="${aloja.imagen}" alt="${aloja.ciudad}" class="img-fluid rounded" style="width: 80px; height: 80px; object-fit: cover;">
@@ -49,7 +49,7 @@ fetch("json/viaje_italia.json")
                                     <h5 class="card-title">${dia.fecha} - ${dia.ciudad}</h5>
                                     <p class="card-text">${actividades
                                       .split("<br>")
-                                      .slice(0, 2)
+                                      .slice(0, 1)
                                       .join("<br>")}...</p>
                                 </div>
                             </div>
